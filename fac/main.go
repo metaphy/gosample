618f30fc08f54ec66ce2bcc97f6fc762530c9b2f
Ming Shi Gou <peter@Mings-MacBook-Air.local>
2019-01-17 11:38:08 +0800

    Calculate Factorial 30 using math/big package

---
+++ fac/main.go
@@ -0,0 +1,25 @@
+/*
+Factorial 30.  We cannot use int or int64 for the result;
+because fac(30) is very large
+*/
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	var n int64
+	for n = 0; n <= 30; n++ {
+		fmt.Printf("Fac(%d)=%d\n", n, fac(n))
+	}
+}
+
+func fac(n int64) *big.Int {
+	if n == 0 {
+		return big.NewInt(1)
+	} else {
+		return big.NewInt(n).Mul(big.NewInt(n), fac(n-1))
+	}
+}
