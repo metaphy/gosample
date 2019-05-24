b22dae1ab92ef75d558a39472ee5f5be79da5667
Peter <peter@Mings-MacBook-Air.local>
2019-05-24 10:46:41 +0800

    drawing

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This project consists of samples of go programming language.
 * **bisearch**: binary search
 * **download**: Download pictures
 * **downloadAllPics**: Download all of the pictures
+* **drawing**: Drawing a PNG on the web
 * **fac**: Calculate Factorial 30, using math/big
 * **fetchurls**: Access the URLs list concurrently
 * **filetools**: File reading and writing
---
+++ drawing/main.go
@@ -0,0 +1,32 @@
+/*
+Drawing
+*/
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"net/http"
+)
+
+var (
+	blue      color.Color = color.RGBA{0, 0, 255, 255}
+	picwidth              = 640
+	picheight             = 480
+)
+
+func main() {
+	http.HandleFunc("/", handl)
+	http.ListenAndServe(":9999", nil)
+}
+
+func handl(rw http.ResponseWriter, req *http.Request) {
+	m := image.NewRGBA(image.Rect(0, 0, picwidth, picheight))
+	// 填充蓝色,并把其写入到m
+	draw.Draw(m, m.Bounds(), &image.Uniform{blue}, image.ZP, draw.Src)
+	//以png编码格式,并将m写入到 rw里面去
+	png.Encode(rw, m) //Encode writes the Image m to w in PNG format.
+
+}
